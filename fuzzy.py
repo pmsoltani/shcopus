cb7c2fd8e50fe444dd555ed2be8dfc927810ece1
Pooria <pooria.ms@gmail.com>
2019-01-07 16:04:09 +0330

    working on extracting co-authors

--- fuzzy.py
+++ fuzzy.py
@@ -1,2 +1,135 @@
-from fuzzy_func2 import *
-analyze_auts('', '', 2018, 'papers.db', datasets, 80, True)
+from fuzzy_func2 import *# analyze_auts, datasets, db_handler
+# analyze_auts('', '', 2018, 'papers.db', datasets, 80, True)
+
+from collections import OrderedDict
+import csv
+
+papers = db_handler('papers.db', 'simchi', '', 2018)
+
+db_name = 'Faculties'
+profs = OrderedDict()
+with open(
+    'datasets/' + db_name + '.csv', encoding='UTF-8-sig'
+) as import_file:
+
+    reader = csv.DictReader(import_file)
+    for row in reader:
+        profs[row['ID']] = {
+            'first': row['First En'],
+            'last': row['Last En'],
+            'init': row['Initial En'],
+            'i_last': row['Initial En'] + ' ' + row['Last En'],
+            'depts': row['Dept'],
+            'co_auts': {}, 'cnt': 0,
+        }
+        if row['Scopus']:
+            profs[row['ID']]['scopus'] = list(
+                map(lambda item: eval(item), row['Scopus'].split(';'))
+            )
+        else:
+            profs[row['ID']]['scopus'] = []
+
+for p, prof in profs.items():
+    if not prof['scopus']:
+        continue
+    # for scop in prof['scopus']:
+    #     print(scop)
+    for i in papers:
+        if type(i['auts_id']) == str:
+            i['auts_id'] = splitter(i['auts_id'], ';', out_type='list')
+            i['auts'] = splitter(
+                i['auts'], ',', vacuum=lambda item: len(item.strip()) > 3)
+            auts_affils = splitter(i['auts_affils'], ';', out_type='list')
+            temp = OrderedDict()
+            for cnt, aut in enumerate(auts_affils):
+                temp[cnt] = {
+                    'raw_affil': aut, 'init': '', 'last': '',
+                    'sharif': False, 'depts': [],
+                    'scopus_id': i['auts_id'][cnt],
+                    'duo_affil': False, 'affils': [], 'countries': [],
+                    'foreign': False, 'foreigner': False,
+                }
+                aut_split = splitter(aut.lower(), ',', out_type='list')
+                [temp[cnt]['last'], temp[cnt]['init']] = aut_split[0:2]
+                [
+                    temp[cnt]['countries'],
+                    temp[cnt]['duo_affil'],
+                    temp[cnt]['foreigner'],
+                    temp[cnt]['foreign'],
+                    temp[cnt]['affils'],
+                ] = aut_country(aut_split, datasets['countries'])
+                if not temp[cnt]['foreigner']:
+                    [
+                        temp[cnt]['sharif'],
+                        temp[cnt]['depts'],
+                    ] = aut_dept(
+                        temp[cnt]['affils'], dept_strings,
+                        sharif_depts, 80, 'sharif'
+                    )
+            i['auts_affils'] = temp
+        if any(scop[0] in i['auts_id'] for scop in prof['scopus']):
+            co_auts = list(filter(lambda item: item not in prof['scopus'], i['auts_id']))
+            for aut in co_auts:
+                idx = i['auts_id'].index(aut)
+                if aut not in prof['co_auts'].keys():
+                    prof['co_auts'][aut] = {
+                        'cnt': 0,
+                        'name'     : i['auts_affils'][idx]['init'] + ' ' + i['auts_affils'][idx]['last'],
+                        'raw_affil': i['auts_affils'][idx]['raw_affil'],
+                        'duo_affil': i['auts_affils'][idx]['duo_affil'],
+                        'countries': i['auts_affils'][idx]['countries'],
+                        'foreigner': i['auts_affils'][idx]['foreigner'],
+                        'foreign'  : i['auts_affils'][idx]['foreign'],
+                        'affils'   : i['auts_affils'][idx]['affils'],
+                        'sharif'   : i['auts_affils'][idx]['sharif'],
+                    }
+                if i['year'] not in prof['co_auts'][aut].keys():
+                    prof['co_auts'][aut][i['year']] = {'cnt': 0, 'doi': []}
+                
+                prof['cnt'] += 1
+
+                prof['co_auts'][aut]['cnt'] += 1
+                prof['co_auts'][aut][i['year']]['cnt'] += 1
+                prof['co_auts'][aut][i['year']]['doi'].append(i['doi'])
+    # prof['co_auts'] = {k: v for k, v in prof['co_auts'].items() if v['cnt'] > 1}
+
+# print(profs['simchi']['co_auts'])
+# print()
+# print()
+# print(len(profs['simchi']['co_auts']))
+# print(profs['simchi'])
+
+# python fuzzy.py
+
+with io.open('co_aut.txt', 'w', encoding='UTF-16') as tsvfile:
+    header = [
+        'First', 'Last', 'Init', 'i_last', 'Depts',
+        'co_auts_id', 'Total', 'Name', 'Raw Affil', 'Double Affil',
+        'Countries', 'Foreigner', 'Foreign Affil', 'Sharif', 'Years', 'DOIs',
+    ]
+    header = '\t'.join(header) + '\n'
+    tsvfile.write(header)
+    for p, prof in profs.items():
+        if prof['co_auts']:
+            exp_prof = [
+                prof['first'], prof['last'], prof['init'], prof['i_last'],
+                prof['depts']
+            ]
+            print(exp_prof)
+            for i, aut in prof['co_auts'].items():
+                exp_list = [
+                    i, aut['cnt'], aut['name'], aut['raw_affil'], aut['duo_affil'],
+                    ';'.join(aut['countries']), aut['foreigner'], aut['foreign'],
+                    aut['sharif'],
+                ]
+                years = {k: v for k, v in aut.items() if '20' in k}
+                dois = [v['doi'] for k, v in years.items()]
+                dois = ';'.join([item for sublist in dois for item in sublist])
+                years = ';'.join([str((k, v['cnt'])) for k, v in years.items()])
+
+                exp_list.append(years)
+                exp_list.append(dois)
+
+                exp_text = '\t'.join(exp_prof + exp_list) + '\n'
+                print(exp_text)
+                tsvfile.write(exp_text)
